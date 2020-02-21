Fix for rotation of non square images

musclex/modules/DiffractionCentroids.py
@@ -202,10 +202,10 @@ class DiffractionCentroids():
         """
         if 'int_area' in self.info:
             return
-        center = self.info['center']
         rmin = self.info['rmin']
         img = getCenterRemovedImage(copy.copy(self.avgImg), self.info['center'], self.info['rmin']) # remove center location
         rotate_img = self.getRotatedImage(img) # rotate image
+        center = self.info['center']
         l = max(0,center[0]-rmin) # initial guess for left line
         r = min(center[0]+rmin, rotate_img.shape[1]) # initial guess for right line
         area = rotate_img[:,l:r] # Get the area by initial guess
@@ -248,8 +248,16 @@ class DiffractionCentroids():
             angle = self.info['rotationAngle']
         if '90rotation' in self.info and self.info['90rotation'] is True:
             angle = angle - 90 if angle > 90 else angle + 90
+            
+        center = self.info["center"]
+        if "orig_center" in self.info:
+            center = self.info["orig_center"]
+        else:
+            self.info["orig_center"] = center
+        
+        rotImg, self.info["center"] = rotateImage(img, center, angle, self.mask_thres)
 
-        return rotateImage(img, self.info["center"], angle, self.mask_thres)
+        return rotImg
 
     def setConvexhullPoints(self):
         """
@@ -282,9 +290,9 @@ class DiffractionCentroids():
         if 'top_hist' in self.info and 'top_hull' in self.info and 'bottom_hist' in self.info and 'bottom_hull' in self.info:
             return
 
-        center_y = self.info['center'][1]
         int_area = self.info['int_area']
         img = self.getRotatedImage(copy.copy(self.avgImg), self.info['rotationAngle'])
+        center_y = self.info['center'][1]
         img_area = img[:,int_area[0]: int_area[1]]
         ignore = np.array([any(line <= self.mask_thres) for line in img_area])
         hist = np.sum(img_area, axis=1)
@@ -581,12 +589,12 @@ class DiffractionCentroids():
         All backgound subtracted histograms will be kept in self.info["off_mer_hists"]["hull"]
         """
         if "off_mer_hists" not in self.info:
-            center_y = self.info["center"][1]
             x1 = self.info["x1"]
             x2 = self.info["x2"]
             x3 = self.info["x3"]
             x4 = self.info["x4"]
             img = self.getRotatedImage()
+            center_y = self.info["center"][1]
             left_area = img[:, x2:x1]
             right_area = img[:, x3:x4]
             # left_ignore = np.array([any(line < 0) for line in left_area])

musclex/modules/QuadrantFolder.py
@@ -185,8 +185,16 @@ class QuadrantFolder(object):
         Get rotated image by angle while image = original input image, and angle = self.info["rotationAngle"]
         """
         img = np.array(self.orig_img, dtype="float32")
-        rotate_img = rotateImage(img,self.info["center"], self.info["rotationAngle"], self.info['mask_thres'])
-        return rotate_img
+        
+        center = self.info["center"]
+        if "orig_center" in self.info:
+            center = self.info["orig_center"]
+        else:
+            self.info["orig_center"] = center
+        
+        rotImg, self.info["center"] = rotateImage(img,center, self.info["rotationAngle"], self.info['mask_thres'])
+        
+        return rotImg
 
     def getFoldNumber(self, x, y):
         """
@@ -712,9 +720,6 @@ class QuadrantFolder(object):
             self.deleteFromDict(self.info, 'rmin')
             self.deleteFromDict(self.info, 'rmax')
             self.imgResultForDisplay = None
-            center = self.info['center']
-            center_x = center[0]
-            center_y = center[1]
             if 'blank_mask' in self.info and self.info['blank_mask']:
                 img = np.array(self.orig_img, 'float32')
                 blank, mask = getBlankImageAndMask(self.img_path)
@@ -726,6 +731,9 @@ class QuadrantFolder(object):
                 rotate_img = rotateImage(img, self.info["center"], self.info["rotationAngle"], self.info['mask_thres'])
             else:
                 rotate_img = copy.copy(self.getRotatedImage())
+            center = self.info['center']
+            center_x = center[0]
+            center_y = center[1]
 
             print("Quadrant folding is being processed...")
             img_width = rotate_img.shape[1]

musclex/utils/image_processor.py
@@ -32,6 +32,7 @@ import numpy as np
 import fabio
 import pyFAI
 from skimage.morphology import white_tophat
+import math
 
 def distance(pt1, pt2):
     """
@@ -488,6 +489,10 @@ def rotateImage(img, center, angle, mask_thres = -999):
     """
     if angle == 0:
         return img
+    
+    if img.shape[0] != img.shape[1]:
+        print("Rotating non square image")
+        return rotateNonSquareImage(img, angle, center)
 
     M = cv2.getRotationMatrix2D(tuple(center), angle, 1)
     size = max(img.shape[0], img.shape[1])
@@ -523,7 +528,7 @@ def rotateImage(img, center, angle, mask_thres = -999):
         rotated_img[rotated_mask > 0] = mask_thres
     else:
         rotated_img = cv2.warpAffine(img, M, (img.shape[1],  img.shape[0]))
-    return rotated_img
+    return rotated_img, center
 
 def rotatePoint(origin, point, angle):
     """
@@ -599,7 +604,7 @@ def averageImages(file_list, rotate=False):
             print("Rotating and centering {}".format(f))
             center = getCenter(img)
             angle = getRotationAngle(img, center, method=0)
-            img = rotateImage(img, center, angle, mask_thres = -999)
+            img, center = rotateImage(img, center, angle, mask_thres = -999)
         all_imgs.append(img)
 
     return np.mean(all_imgs, axis=0)
@@ -620,3 +625,39 @@ def processImageForIntCenter(img, center):
     translated_Img = cv2.warpAffine(img,M,(cols,rows))
    
     return (translated_Img, int_Center)
+
+def rotateNonSquareImage(img, angle, center1):
+    """
+    Rotates a non square image by first determining the appropriate square image and then rotating the image.
+    :param file_list: original non square image, angle of rotation and center
+    :return: rotated image and center with respect to new coordinate system
+    """
+    print("initial center is " + str(center1))
+    height, width = img.shape
+    center = (width/2, height/2)
+
+    rotation_mat = cv2.getRotationMatrix2D(center, angle, 1.)
+
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotation_mat[0,0]) 
+    abs_sin = abs(rotation_mat[0,1])
+
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotation_mat[0, 2] += bound_w/2 - center[0]
+    rotation_mat[1, 2] += bound_h/2 - center[1]
+
+    maxB = max(bound_h, bound_w)
+
+    center1 = [center1[0], center1[1], 1]
+    center1 = np.dot(rotation_mat, center1)
+    center2 = (int(center1[0]), int(center1[1]))
+    
+    print("New center after rotation is " + str(center2))
+    
+    # rotate image with the new bounds and translated rotation matrix
+    rotated_img = cv2.warpAffine(img, rotation_mat, (maxB, maxB))
+    return rotated_img, center2