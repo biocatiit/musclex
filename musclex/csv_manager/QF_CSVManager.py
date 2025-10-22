5305044b835c9452cc15d216ccc9c263f32f339e
Alex <alexlanxy@gmail.com>
2025-10-22 12:09:01 -0500

    Enhance center retrieval logic in QF_CSVManager

--- musclex/csv_manager/QF_CSVManager.py
+++ musclex/csv_manager/QF_CSVManager.py
@@ -103,8 +103,17 @@ class QF_CSVManager:
                 center = quadFold.info['calib_center']
             elif 'manual_center' in quadFold.info:
                 center = quadFold.info['manual_center']
-            else:
+            elif 'auto_center' in quadFold.info:
+                # New mechanism: use cached auto center
+                center = quadFold.info['auto_center']
+            elif quadFold.orig_image_center is not None:
                 center = (round(quadFold.orig_image_center[0], 2), round(quadFold.orig_image_center[1], 2))
+            elif 'center' in quadFold.info:
+                # Final fallback: use whatever center is in info
+                center = quadFold.info['center']
+            else:
+                # Should never happen, but provide a safe fallback
+                center = (0, 0)
             # Get all needed infos
             data['Filename'] = img_name
             data['centerX'] = center[0]
