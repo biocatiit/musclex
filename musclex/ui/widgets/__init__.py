ec95668d3b7365ccf238f63c4a8f7839c9a7ac53
Alex <alexlanxy@gmail.com>
2026-02-11 11:29:34 -0600

    Remove unused ZoomInWidget and ZoomHandler classes to streamline widget imports and improve code maintainability.

--- musclex/ui/widgets/__init__.py
+++ musclex/ui/widgets/__init__.py
@@ -1,9 +1,7 @@
 # Package marker for ui.widgets
 from .navigation_controls import NavigationControls
 from .double_zoom_widget import DoubleZoomWidget
-from .zoomin_widget import ZoomInWidget, ZoomInWidgetState
 from .image_mouse_move_handler import ImageMouseMoveHandler, ImageMouseMoveState
-from .zoom_handler import ZoomHandler
 from .ui_widget import UIWidget
 from .display_options_panel import DisplayOptionsPanel
 from .collapsible_right_panel import CollapsibleRightPanel
@@ -16,11 +14,8 @@ from .parameter_editor_table import ParameterEditorTable
 __all__ = [
     'NavigationControls',
     'DoubleZoomWidget',
-    'ZoomInWidget',
-    'ZoomInWidgetState',
     'ImageMouseMoveHandler',
     'ImageMouseMoveState',
-    'ZoomHandler',
     'UIWidget',
     'DisplayOptionsPanel',
     'CollapsibleRightPanel',
