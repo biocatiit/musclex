624adb3291be2a6e62bb7f5968b756f86c1715f9
Alex <alexlanxy@gmail.com>
2026-04-17 16:15:18 -0500

    Update version to 1.27.6 across all relevant files

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ copyright = '2025, BioCAT'
 author = 'BioCAT'
 
 version = '1.27'
-release = '1.27.5'
+release = '1.27.6'
 
 # -- General configuration ---------------------------------------------------
 
--- docs/index.rst
+++ docs/index.rst
@@ -22,7 +22,7 @@ assist with analyzing diffraction X-ray images.
 
 .. rubric:: Get the software
 
-- Current stable version: 1.27.5
+- Current stable version: 1.27.6
 - :ref:`Get Muscle X <Install>`
 
 
